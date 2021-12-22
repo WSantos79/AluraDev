f7a5a467e6cc85ec5d4e18dac904d2f82b5a3226
WSantos79 <58752564+WSantos79@users.noreply.github.com>
2021-12-22 09:20:21 -0800

    add menu do mobile

--- css/comunidade.js
+++ css/comunidade.js
@@ -1,31 +1,3 @@
-/*  --------------------Eu tentando barra de menu smart ------------------------------  */
-let i = 0;
-const abriMenu = (evento) => {
-  evento.preventDefault();
-
-  const imgMenu = document.querySelector('[data-img-menu]');
-  const menu = document.querySelector('.menu-principal');
-  const barraMenu = document.querySelector('.menu-smart');
-  const usuario = document.querySelector('.profile');
-
-  if (i == 0){ 
-  menu.style.display = `block`;
-  barraMenu.style.display = `block`;
-  usuario.style.display = `block`;
-  imgMenu.setAttribute('src', 'img/close-icon.svg');
-  i = 1;
-
-  }else{
-    menu.style.display = `none`;
-    barraMenu.style.display = `none`;
-    usuario.style.display = `none`;
-    imgMenu.setAttribute('src', 'img/menu-icon.svg');
-    i = 0;
-  }
-}
-const menu = document.querySelector('.icon-menu');
-menu.addEventListener('click', abriMenu);
-
 /*  ------------------ botao amei ------------------------------------  */
 const btnLoveUp = document.querySelectorAll('.love-up-efeito');
 const  up = document.querySelectorAll('.love-up');
--- css/index.js
+++ css/index.js
@@ -1,3 +1,62 @@
+const menu = document.querySelector('.icon-menu');
+const iconBusca = document.querySelector('[data-search]');
+
+
+/*  --------------------barra de search mobile ------------------------------  */
+let x = 0;
+const barraPesquisa = (evento) =>  {
+  evento.preventDefault();    
+
+  const logoAlura = document.querySelector('[data-logo-alura]');
+  const input = document.querySelector('.input-busca');
+  if(x == 0){
+    iconBusca.setAttribute('src', 'img/close-icon.svg');
+    iconBusca.style.margin = `0`;
+    input.style.display = `block`;
+    logoAlura.style.display = `none`;
+    menu.style.display = `none`;
+    x = 1;
+  }else{
+    iconBusca.setAttribute('src', 'img/search-icon.svg');
+    iconBusca.style.margin = `0 1rem`;
+    input.style.display = `none`;
+    logoAlura.style.display = `block`;
+    menu.style.display = `block`;
+    x = 0;
+  }
+}
+
+iconBusca.addEventListener('click', barraPesquisa);
+
+/*  --------------------barra de menu smart mobile------------------------------  */
+let i = 0;
+
+const abriMenu = (evento) => {
+  evento.preventDefault();
+
+  const imgMenu = document.querySelector('[data-img-menu]');
+  const menuTotal = document.querySelector('.menu-principal');
+  const barraMenu = document.querySelector('.menu-smart');
+  const usuario = document.querySelector('.profile');
+
+  if (i == 0){ 
+  menuTotal.style.display = `block`;
+  barraMenu.style.display = `block`;
+  usuario.style.display = `block`;
+  imgMenu.setAttribute('src', 'img/close-icon.svg');
+  i = 1;
+
+  }else{
+    menuTotal.style.display = `none`;
+    barraMenu.style.display = `none`;
+    usuario.style.display = `none`;
+    imgMenu.setAttribute('src', 'img/menu-icon.svg');
+    i = 0;
+  }
+}
+
+menu.addEventListener('click', abriMenu);
+
 /*  -------------------- TAB dentro do text area ------------------------------  */
 document.getElementById('codigo').addEventListener('keydown', function (e) {
   if (e.keyCode === 9) { // TAB
@@ -48,32 +107,3 @@ btnSalvar.addEventListener('click', salvarProjeto);
 
 
 
-/*  --------------------Eu tentando barra de menu smart ------------------------------  */
-let i = 0;
-const abriMenu = (evento) => {
-  evento.preventDefault();
-
-  const imgMenu = document.querySelector('[data-img-menu]');
-  const menu = document.querySelector('.menu-principal');
-  const barraMenu = document.querySelector('.menu-smart');
-  const usuario = document.querySelector('.profile');
-
-  if (i == 0){ 
-  menu.style.display = `block`;
-  barraMenu.style.display = `block`;
-  usuario.style.display = `block`;
-  imgMenu.setAttribute('src', 'img/close-icon.svg');
-  i = 1;
-
-  }else{
-    menu.style.display = `none`;
-    barraMenu.style.display = `none`;
-    usuario.style.display = `none`;
-    imgMenu.setAttribute('src', 'img/menu-icon.svg');
-    i = 0;
-  }
-}
-
-const menu = document.querySelector('.icon-menu');
-
-menu.addEventListener('click', abriMenu);
